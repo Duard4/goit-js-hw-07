e7d06444550eb1215c1c8a52603e0e33dca7f090
Eduard <parhomenkoeduard75@gmail.com>
2024-09-24 21:41:16 +0300

    fix

--- js/task-4.js
+++ js/task-4.js
@@ -8,13 +8,14 @@ function handleSubmit(event) {
     const email = form.elements.email.value.trim();
     const password = form.elements.password.value.trim();
     if (!(email && password)) alert('All form fields must be filled in');
-
-    const output =  {
-        email,
-        password
-    };
-    console.log(output);
-    form.reset();
+    else {
+        const output =  {
+            email,
+            password
+        };
+        console.log(output);
+        form.reset();
+    }
 }
 
 form.addEventListener("submit", handleSubmit);
